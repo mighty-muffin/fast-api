8bddc10df96a420e0849654cae9b4e59936a9fc8
Simon Harvey <simon.harvey.86@gmail.com>
2025-05-23 20:57:27 -0400

    ci: add test pipeline

--- fast-api/vuln/vuln.py
+++
@@ -1,5 +0,0 @@
-import pickle
-
-serialized_data = input("Enter serialized data: ")
-deserialized_data = pickle.loads(serialized_data.encode('latin1'))
-print("Deserialized data:", deserialized_data)
